00bdc8b0bdfe84daba0f9ae17a426a6963c62583
Sagar Rokade <sagarrokade006@gmail.com>
2018-05-29 08:51:51 +0000

    Some Changes

--- lluvia.mk
+++ lluvia.mk
@@ -17,7 +17,7 @@ $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/lluvia/config/commo_full_phone.mk)
+$(call inherit-product, vendor/lluvia/config/common_full_phone.mk)
 $(call inherit-product, vendor/lluvia/config/caf_fw.mk)
 
 
